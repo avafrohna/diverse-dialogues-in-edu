cb862aeb725d07a82942d8f47e326cfcf803b421
Ava Frohna <122284870+avafrohna@users.noreply.github.com>
2024-11-10 14:55:46 +1000

    basic setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './components/header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <header className="App-header">
+        <p>
+          This is the code for my website.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header> 
+            <div>This is the header</div>
+        </header>
+    );
+}
+
+export default Header;
